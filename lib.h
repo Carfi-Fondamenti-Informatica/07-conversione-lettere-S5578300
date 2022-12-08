3b316016b0cbc4e385ea6392afe489aa0b48ee1f
S5578300 <118068983+S5578300@users.noreply.github.com>
2022-12-09 00:18:48 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,5 @@
-
+#ifndef PRIMALIBRERIA_PRIMALIBRERIA_H
+#define PRIMALIBRERIA PRIMALIBRERIA H
+bool letteraono(int valore);
+char conversionelettere(char valore);
+#endif //PRIMALIBRERIA PRIMALIBRERIA H
